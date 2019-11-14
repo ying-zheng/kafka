37767f70c9e8d48a9bec93156d6b220b66a04958
Ying Zheng <yingz@uber.com>
2019-11-13 16:29:03 -0800

    Remove next local offset from fetch response

--- core/src/main/scala/kafka/server/ReplicaFetcherThread.scala
+++ core/src/main/scala/kafka/server/ReplicaFetcherThread.scala
@@ -66,7 +66,7 @@ class ReplicaFetcherThread(name: String,
 
   // Visible for testing
   private[server] val fetchRequestVersion: Short =
-    if (brokerConfig.interBrokerProtocolVersion >= KAFKA_2_3_IV1) 12
+    if (brokerConfig.interBrokerProtocolVersion >= KAFKA_2_3_IV1) 11
     else if (brokerConfig.interBrokerProtocolVersion >= KAFKA_2_1_IV2) 10
     else if (brokerConfig.interBrokerProtocolVersion >= KAFKA_2_0_IV1) 8
     else if (brokerConfig.interBrokerProtocolVersion >= KAFKA_1_1_IV0) 7
